89c09ec518d17dc0823dbf53bb87b5a2fd349a84
Ishan <ishanwijes@gmail.com>
2022-09-20 20:08:30 +1000

    async-await in useEffect()

--- src/components/WindowTracker.js
+++ src/components/WindowTracker.js
@@ -17,6 +17,9 @@ export const WindowTracker = function() {
         // whenever a component stops being rendered in the HTML, this function is executed automatically
         // no other code in the component will be run when a component STOPS being rendered, only what's in these cleanup functions
         // this is where you are suppose to cleanup after yourself, for instance, if you add an event listener to something inside a component, when that component stops being rendered, that event listener is still active, the cleanup function is where you remove that event listener
+
+        // this is the main reason we can't use async-await for the callback function of useEffect(), because when we declare a function an async function, what is returned is a promise, and therefore you can't return a cleanup function
+        // an easy way around this is to create a function inside useEffect's callback function, and make that an async function, then call it, then return a cleanup function
         return () => {
             // to remove event listener, it needs to have the exact same arguments as the addEventListener
             window.removeEventListener('resize', windowResizeFunction)
